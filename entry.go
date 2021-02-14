1fab424eeb4b85b15643a162bcfef0ec09ba3809
Peter Arthur <pete.arthur@gmail.com>
2021-02-14 17:42:22 +0000

    Add version flag

--- entry.go
+++ entry.go
@@ -2,20 +2,30 @@ package kana
 
 import (
 	"flag"
+	"fmt"
 	"os"
 )
 
+// Version - this version of kana
+const Version = "kana v0.1"
+
 // Main entrypoint for main Kana interpreter
 func Main() {
-	var help bool
+	var help, version bool
 	var path string
 	flag.BoolVar(&help, "help", false, "Show help")
+	flag.BoolVar(&version, "version", false, "Show the current version")
 	flag.StringVar(&path, "path", "", "add directories to kana load path")
 
 	flag.Parse()
 	if help {
 		flag.Usage()
 		os.Exit(1)
+	}
+
+	if version {
+		fmt.Println(Version)
+		os.Exit(0)
 	}
 	args := flag.Args()
 
