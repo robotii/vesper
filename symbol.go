fb06cb3e6b44198614299b56c2fa059e42b38c0b
Peter Arthur <pete.arthur@gmail.com>
2021-02-14 21:10:42 +0000

    Add IsSymbol and IsKeyword function

--- symbol.go
+++ symbol.go
@@ -1,5 +1,15 @@
 package kana
 
+// IsSymbol returns true if the object is a symbol
+func IsSymbol(obj *Object) bool {
+	return obj.Type == SymbolType
+}
+
+// IsKeyword returns true if the object is a keyword
+func IsKeyword(obj *Object) bool {
+	return obj.Type == KeywordType
+}
+
 // Intern puts a string into the global environment
 func Intern(name string) *Object {
 	// TODO: Implement
