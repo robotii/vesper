9161cbad7b8ee65105739ffd85ba4226cfd561cf
Peter Arthur <pete.arthur@gmail.com>
2021-02-23 19:19:56 +0000

    Add MakeCode function

--- bytecode.go
+++ bytecode.go
@@ -15,6 +15,20 @@ type Code struct {
 func IsCode(obj *Object) bool {
 	return obj.Type == CodeType
 }
+
+// MakeCode creates a new code object
+func MakeCode(argc int, defaults []*Object, keys []*Object, name string) *Object {
+	return &Object{
+		Type: CodeType,
+		code: &Code{
+			name:     name,
+			ops:      nil,
+			argc:     argc,
+			defaults: defaults, //nil for normal procs, empty for rest, and non-empty for optional/keyword
+			keys:     keys,
+		},
+	}
+}
 func (code *Code) String() string {
 	// TODO: Better string representation
 	return fmt.Sprintf("(function (%d %v %s) %v)", code.argc, code.defaults, code.keys, code.ops)
