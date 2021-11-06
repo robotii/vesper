09f88e7e60aafcab5bdadcdd1e9de0d99f41062f
Peter Arthur <pete.arthur@gmail.com>
2021-11-06 22:08:31 +0000

    Code cleanup in boolean.go

--- boolean.go
+++ boolean.go
@@ -1,13 +1,13 @@
 package vesper
 
-// BooleanType is the type of true and false
-var BooleanType = defaultVM.Intern("<boolean>")
-
-// True is the singleton boolean true value
-var True = &Object{Type: BooleanType, fval: 1}
-
-// False is the singleton boolean false value
-var False = &Object{Type: BooleanType, fval: 0}
+var (
+	// BooleanType is the type of true and false
+	BooleanType = defaultVM.Intern("<boolean>")
+	// True is the singleton boolean true value
+	True = &Object{Type: BooleanType, fval: 1}
+	// False is the singleton boolean false value
+	False = &Object{Type: BooleanType, fval: 0}
+)
 
 // IsBoolean returns true if the object type is boolean
 func IsBoolean(obj *Object) bool {
